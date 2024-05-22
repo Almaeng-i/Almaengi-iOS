0232ac80906443d5f986c20e2d199c6d08518c51
새미 <s22058@gsm.hs.kr>
2024-05-22 20:52:42 +0900

    💄 :: Color Design System

---
+++ Projects/Feature/Sources/DesignSystem/Color/Color+Almaengi.swift
@@ -0,0 +1,40 @@
+//
+//  Color+Almaengi.swift
+//  Feature
+//
+//  Created by 새미 on 5/22/24.
+//
+
+import SwiftUI
+
+extension Color {
+    // MARK: Primary
+    static let primary1 = Color(.P_1)
+    static let primary2 = Color(.P_2)
+    static let primary3 = Color(.P_3)
+    static let primary4 = Color(.P_4)
+    static let primary5 = Color(.P_5)
+    
+    // MARK: - Gray
+    static let grayscale1 = Color(.G_1)
+    static let grayscale2 = Color(.G_2)
+    static let grayscale3 = Color(.G_3)
+    static let grayscale4 = Color(.G_4)
+    static let grayscale5 = Color(.G_5)
+    static let grayscale6 = Color(.G_6)
+    static let grayscale7 = Color(.G_7)
+    static let grayscale8 = Color(.G_8)
+    static let grayscale9 = Color(.G_9)
+    
+    // MARK: - System
+    static let systemRed = Color(.SYSTEM_1)
+    static let systemYellow = Color(.SYSTEM_2)
+    static let systemGreen = Color(.SYSTEM_3)
+    
+    // MARK: - Sub
+    static let subPink = Color(.SUB_1)
+    static let subOrange = Color(.SUB_2)
+    static let subYellow = Color(.SUB_3)
+    static let subGreen = Color(.SUB_4)
+    static let subPurple = Color(.SUB_5)
+}
