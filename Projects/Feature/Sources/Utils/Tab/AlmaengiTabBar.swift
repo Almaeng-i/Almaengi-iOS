248fe7eb0e1860d6f51520b4286aa5d102774d37
saemiee <s22058@gsm.hs.kr>
2024-06-17 11:07:02 +0900

    🔥 :: Delete TabBar Navigation

--- Projects/Feature/Sources/Utils/Tab/AlmaengiTabBar.swift
+++ Projects/Feature/Sources/Utils/Tab/AlmaengiTabBar.swift
@@ -30,29 +30,28 @@ public struct AlmaengiTabBar: View {
     public init() { }
     
     public var body: some View {
-        AlmaengiNavView {
+        ZStack {
             ZStack {
-                ZStack {
-                    switch selectedIndex {
-                    case 0:
-                        MainView()
-                    case 1:
-                        ListView()
-                    case 2:
-                        Image(asset: FeatureAsset.Images.Tab.UnSelected.home)
-                    case 3:
-                        PushView()
-                    case 4:
-                        MypageView()
-                    default:
-                        Image(asset: FeatureAsset.Images.Tab.UnSelected.home)
-                    }
+                switch selectedIndex {
+                case 0:
+                    MainView()
+                case 1:
+                    ListView()
+                case 2:
+                    Image(asset: FeatureAsset.Images.Tab.UnSelected.home)
+                case 3:
+                    PushView()
+                case 4:
+                    MypageView()
+                default:
+                    Image(asset: FeatureAsset.Images.Tab.UnSelected.home)
                 }
-                VStack {
-                    Spacer()
-                    HStack(spacing: 0) {
-                        ForEach(selectedImage.indices, id: \.self) { index in
-                            (selectedIndex == index ? AnyView(selectedImage[index]) : AnyView(unSelectedImage[index]))
+            }
+            VStack {
+                Spacer()
+                HStack(spacing: 0) {
+                    ForEach(selectedImage.indices, id: \.self) { index in
+                        (selectedIndex == index ? AnyView(selectedImage[index]) : AnyView(unSelectedImage[index]))
                             .frame(width: 36, height: 36)
                             .padding(.horizontal,
                                      ((UIScreen.main.bounds.width) * 0.05)
@@ -63,15 +62,14 @@ public struct AlmaengiTabBar: View {
                                         selectedIndex = index
                                     }
                             )
-                        }
                     }
-                    .padding(.top, 8)
-                    .padding(.bottom, 42)
-                    .background(selectedIndex == 0 ? .clear : .white)
                 }
+                .padding(.top, 8)
+                .padding(.bottom, 42)
+                .background(selectedIndex == 0 ? .clear : .white)
             }
-            .edgesIgnoringSafeArea(.bottom)
-            .AlmaengiNavigationBackButtonHidden(true)
         }
+        .edgesIgnoringSafeArea(.bottom)
+        .AlmaengiNavigationBackButtonHidden(true)
     }
 }
