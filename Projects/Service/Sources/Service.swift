ae5fc8537459cb35c1c48993760ad3f656dfb5b2
saemiee <s22058@gsm.hs.kr>
2024-07-01 18:22:41 +0900

    ✨ :: Add User Profile Model

--- Projects/Service/Sources/Service.swift
+++ Projects/Service/Sources/Service.swift
@@ -5,4 +5,12 @@
 //  Created by 새미 on 5/22/24.
 //
 
-import Foundation
+public struct UserProfileModel: Decodable {
+    let date: UserProfileMode
+}
+
+public struct UserProfileMode: Decodable {
+    public let userName: String
+    public let userIntroduction: String
+    public let userImage: String
+}
