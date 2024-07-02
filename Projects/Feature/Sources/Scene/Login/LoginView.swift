e4cb392ce537d0a35054a08403860d86c82feda9
saemiee <s22058@gsm.hs.kr>
2024-07-02 20:56:09 +0900

    💄 :: LoginView UI 수정

--- Projects/Feature/Sources/Scene/Login/LoginView.swift
+++ Projects/Feature/Sources/Scene/Login/LoginView.swift
@@ -14,36 +14,37 @@ public struct LoginView: View {
     public init() {}
     
     public var body: some View {
-            VStack {
-                VStack(alignment: .leading, spacing: 0) {
-                    PotatoView()
-                        .padding(.leading, 77)
-                    Text(mainText)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(6)
-                        .padding(.vertical, 6)
-                        .foregroundColor(.g9)
-                        .font(Font(FeatureFontFamily.Pretendard.bold.font(size: 26)))
-                }
-                .padding(.top, 128)
-                .frame(maxWidth: .infinity)
-                Spacer()
-                Image(asset: FeatureAsset.Images.Almaengi.Color.Sprout.sprout)
-                    .resizable()
-                    .frame(width: 222, height: 193)
-                Spacer()
-                VStack(spacing: 12) {
-                    KakaoSigninButton {
-                        self.isLogin = true
-                    }
-                    Image(asset: FeatureAsset.Images.LoginButton.apple)
-                }
-                .fullScreenCover(isPresented: $isLogin, content: {
-                    AlmaengiTabBar()
-                })
-                .padding(.bottom, 24)
+        VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
+                PotatoView()
+                    .padding(.leading, 77)
+                Text(mainText)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(6)
+                    .padding(.vertical, 6)
+                    .foregroundColor(.g9)
+                    .font(Font(FeatureFontFamily.Pretendard.bold.font(size: 26)))
             }
-            .padding(.horizontal, 24)
+            .padding(.top, 128)
+            .padding(.bottom, 24)
+            .frame(maxWidth: .infinity)
+            Image(asset: FeatureAsset.Images.Almaengi.Color.Sprout.sprout)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250)
+            Spacer()
+            VStack(spacing: 12) {
+                Image(asset: FeatureAsset.Images.LoginButton.apple)
+                KakaoSigninButton {
+                    self.isLogin = true
+                }
+            }
+            .padding(.vertical, 24)
+            .fullScreenCover(isPresented: $isLogin, content: {
+                AlmaengiTabBar()
+            })
+        }
+        .padding(.horizontal, 24)
     }
 }
 
